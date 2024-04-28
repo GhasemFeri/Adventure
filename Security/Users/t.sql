88082cd4abdfd2f035a61eb4b1444dc6c3f39328
Ghasem Feri <gh.fereydooni1988@gmail.com>
2024-04-28 13:22:23 +0330

    t

---
+++ Security/Users/t.sql
@@ -0,0 +1,5 @@
+IF NOT EXISTS (SELECT * FROM master.dbo.syslogins WHERE loginname = N'NT SERVICE\Winmgmt')
+CREATE LOGIN [NT SERVICE\Winmgmt] FROM WINDOWS
+GO
+CREATE USER [t] FOR LOGIN [NT SERVICE\Winmgmt]
+GO
